c13b86fc4d7282f20bf5ca447605496df6312621
Timur123415 <106232712+Timur123415@users.noreply.github.com>
2024-02-05 12:00:07 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
     port: 8080
   },
   module: {
-<<<<<<< HEAD
     rules: [ 
       {
         test: /\.js$/, 
@@ -42,35 +41,30 @@ module.exports = {
           options: { importLoaders: 1 } 
         },
         'postcss-loader'] 
-=======
-    rules: [ // массив правил
-      // Babel
+    rules: [ 
       {
-        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-        use: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
-        exclude: '/node_modules/' // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        test: /\.js$/, 
+        use: 'babel-loader', 
+        exclude: '/node_modules/' 
       },
 
-      // Изображения и шрифты
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource' // переносить исходные файлы в конечную сборку в том же формате
+        type: 'asset/resource' 
       },
 
-      // MiniCssExtractPlugin.loader и css-loader
+    
       {
-        test: /\.css$/, // применять это правило только к CSS-файлам
+        test: /\.css$/, 
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
-          options: { importLoaders: 1 } // изменение порядка запуска, чтобы правильно работали импорты в css (сначала обработка в PostCSS)
+          options: { importLoaders: 1 }
         },
-        'postcss-loader'] // PostCSS
->>>>>>> fc547fb77c5b120641b73da97bf9e27b1484a261
+        'postcss-loader'] 
       },
     ]
   },
 
-<<<<<<< HEAD
 
   plugins: [
     new HtmlWebpackPlugin({
@@ -78,14 +72,11 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(), 
-=======
-  // Плагины
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // путь к файлу index.html
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(), // подключение MiniCssExtractPlugin
->>>>>>> fc547fb77c5b120641b73da97bf9e27b1484a261
+    new MiniCssExtractPlugin(), 
   ]
 } 
