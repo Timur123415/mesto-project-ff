553af735e75b95928ff6da9d3b5e8eaa6b1c5409
Timur123415 <106232712+Timur123415@users.noreply.github.com>
2024-02-05 12:04:49 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,6 +1,5 @@
 export const formEditProfile = document.forms['edit-profile'];
 export const formAddCard = document.forms['new-place'];
-<<<<<<< HEAD
 export const formEditAvatar = document.forms['edit-avatar'];
 export const profileImg = document.querySelector('.profile__image');
 export const profileTitle = document.querySelector('.profile__title');
@@ -8,20 +7,15 @@ export const profileDescription = document.querySelector(
   '.profile__description'
 );
 export const cardContainer = document.querySelector('.places__list');
-=======
 export const profileTitle = document.querySelector('.profile__title');
 export const profileDescription = document.querySelector('.profile__description');
 export const cardContainer = document.querySelector('.places__list'); 
->>>>>>> fc547fb77c5b120641b73da97bf9e27b1484a261
 export const popupImage = document.querySelector('.popup_type_image');
 export const popupImageLink = document.querySelector('.popup__image');
 export const popupImageName = document.querySelector('.popup__caption');
 export const profileEditPopup = document.querySelector('.popup_type_edit');
-<<<<<<< HEAD
 export const addCardPopup = document.querySelector('.popup_type_new-card');
 export const changeAvatarPopup = document.querySelector(
   '.popup_type_edit-avatar'
 );
-=======
 export const addCardPopup = document.querySelector('.popup_type_new-card');
->>>>>>> fc547fb77c5b120641b73da97bf9e27b1484a261
